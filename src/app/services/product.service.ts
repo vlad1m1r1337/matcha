7faf840fe5a3bf587dab6d31557dfbcf0e696797
vladimir <vladim1r.gribkov@yandex.ru>
2026-01-06 20:50:09 +0400

    add readme.md

--- src/app/services/product.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
-
-@Injectable({ providedIn: 'root' })
-export class ProductService {
-  getProductById(id: number) {
-    return of({ id, name: `Товар ${id}`, price: id * 10 });
-  }
-}
